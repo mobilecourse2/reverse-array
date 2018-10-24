2ec94c7b1023d2add36389147afc8406238408ec
Mobile Application Course <44363418+mobilecourse2@users.noreply.github.com>
2018-10-24 12:34:22 +0500

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,20 @@
+public class reverse{
+	public static void main(String[] args){
+		
+		
+		int []array={7,9,3,4,5};
+		int arr[] = new int [array.length];
+		int s = 4;
+		for(int i=0;i<array.length;i++){
+			 arr[i] = array[s];
+			 s--;
+			
+			
+			
+			System.out.print(arr[i]);
+		}
+		}
+		
+		
+	}
+	
